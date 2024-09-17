3fd655975979dbd4cbcdd1ebd3ae5d32b316ed00
pengpeng404 <jiaopengpengcn@foxmail.com>
2024-09-17 20:45:35 +0800

    Add leetcode q1 two sum

---
+++ LeetcodeTopQuestions/class01/Problem_0001_TwoSum/Problem_0001_TwoSum.go
@@ -0,0 +1,18 @@
+package main
+
+// https://leetcode.com/problems/two-sum/
+func main() {
+
+}
+func twoSum(nums []int, target int) []int {
+	hp := make(map[int]int)
+	for i := range nums {
+		val, exist := hp[target-nums[i]]
+		if exist {
+			return []int{val, i}
+		} else {
+			hp[nums[i]] = i
+		}
+	}
+	return []int{-1, -1}
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # zuoAlgo-Go
+
+---
+
+[Leetcode高频题目全讲（1 - 26）](https://github.com/pengpeng404/zuoAlgo-Go/tree/main/LeetcodeTopQuestions)
