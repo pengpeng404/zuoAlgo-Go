a52dd0b385695438e296b9c8ee6f07296b63c9ec
pengpeng404 <jiaopengpengcn@foxmail.com>
2025-03-04 13:43:08 +0800

    add: 荷兰国旗问题

--- SystematicStudyProgram/coding/coding.go
+++ SystematicStudyProgram/coding/coding.go
@@ -1,68 +1,42 @@
 package main
 
-import "fmt"
-
-/**
- * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
- *
- *
- * @param nums int整型一维数组
- * @return int整型
- */
+import (
+	"fmt"
+)
 
 func main() {
-	arr := []int{1, 3, 2, 4, 1}
-	fmt.Println(InversePairs(arr))
+	arr := []int{3, 5}
+	fmt.Println(netherlandsFlag(arr, 0, len(arr)-1))
+	fmt.Println(arr)
 }
 
-func InversePairs(nums []int) int {
-	// write code here
-	if len(nums) < 2 {
-		return 0
-	}
-	return process(nums, 0, len(nums)-1)
-}
+// 荷兰国旗问题
 
-func process(arr []int, l, r int) int {
+func netherlandsFlag(arr []int, l, r int) []int {
 	if l == r {
-		return 0
+		return []int{l, r}
 	}
-	mid := l + (r-l)>>1
-	num1 := process(arr, l, mid)
-	num2 := process(arr, mid+1, r)
-	num3 := merge(arr, l, mid, r)
-	return num1 + num2 + num3
+	less := l - 1
+	more := r
+	index := l
+	for index < more {
+		if arr[index] < arr[r] {
+			swap(arr, index, less+1)
+			less++
+			index++
+		} else if arr[index] == arr[r] {
+			index++
+		} else {
+			swap(arr, index, more-1)
+			more--
+		}
+	}
+	swap(arr, r, more)
+	return []int{less + 1, more}
 }
 
-func merge(arr []int, l, mid, r int) int {
-	help := make([]int, r-l+1)
-	index := 0
-	p1 := l
-	p2 := mid + 1
-	res := 0
-	for p1 <= mid && p2 <= r {
-		if arr[p1] > arr[p2] {
-			help[index] = arr[p2]
-			res += r - p2 + 1
-			p2++
-		} else {
-			help[index] = arr[p1]
-			p1++
-		}
-		index++
-	}
-	for p1 <= mid {
-		help[index] = arr[p1]
-		p1++
-		index++
-	}
-	for p2 <= r {
-		help[index] = arr[p2]
-		p2++
-		index++
-	}
-	for i := 0; i < len(help); i++ {
-		arr[l+i] = help[i]
-	}
-	return res
+func swap(arr []int, i, j int) {
+	tmp := arr[i]
+	arr[i] = arr[j]
+	arr[j] = tmp
 }
