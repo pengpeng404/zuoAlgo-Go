1b8bdd250f5d57c8306ab66fc23d6013940a68bc
pengpeng404 <jiaopengpengcn@foxmail.com>
2025-03-04 13:50:44 +0800

    add: 荷兰国旗问题

--- SystematicStudyProgram/coding/coding.go
+++ SystematicStudyProgram/coding/coding.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
-	arr := []int{3, 5}
-	fmt.Println(netherlandsFlag(arr, 0, len(arr)-1))
+	arr := []int{3, 5, 7, 3, 6, 9, 6, 5, 7, 1, 2, 9, 7, 6, 7, 9, 8, 7, 6, 5, 6, 7, 8, 9, 3, 4, 23, 2, 4, 435, 6, 7, 7}
+	process(arr, 0, len(arr)-1)
 	fmt.Println(arr)
+}
+
+func process(arr []int, l, r int) {
+	if l >= r {
+		return
+	}
+	index := rand.Intn(r - l + 1)
+	swap(arr, l+index, r)
+	area := netherlandsFlag(arr, l, r)
+	process(arr, l, area[0]-1)
+	process(arr, area[1]+1, r)
 }
 
 // 荷兰国旗问题
